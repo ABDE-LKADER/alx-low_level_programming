deeeb0bf280239d4f3b619b7b92ebaaa6b79da28
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-17 09:51:52 -0800

    Lowercase Alphabet Reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'z';
+	while (alphabet >= 'a')
+	{
+		putchar(alphabet);
+		alphabet--;
+	}
+	putchar('\n');
+	return (0);
+}
