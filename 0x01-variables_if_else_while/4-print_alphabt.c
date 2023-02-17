b9455b5d6a9db384ab895d6778d2a26c06976e8d
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-17 09:38:51 -0800

    Alphabet Lowercase Expect 'q' And 'e'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a';
+	while (alphabet <= 'z')
+	{
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet);
+		}
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
