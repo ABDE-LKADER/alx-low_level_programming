7bceed75e8310754a9fcab74e9e9c52962bb6745
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-17 06:59:32 -0800

    Last Digit Of Number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/*l is the last digit of number n*/
 	l = n % 10;
-	if (l < 6)
+	if (l < 6 && l != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 	}
