ae79b2bdbaa6f265b0359ffd9152a5a7e20fff20
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-17 07:20:46 -0800

    Last Digit Of Number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	}
 	else if (l > 5)
 	{
-		printf("Last digit of %d is %d and is graeter than 5\n", n, l);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
 	}
 	else
 		printf("Last digit of %d is %d and is 0\n", n, l);
