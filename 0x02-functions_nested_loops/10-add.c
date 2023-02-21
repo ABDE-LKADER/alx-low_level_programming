11f71bff047c6268b5b558b4613a3c6ebb879ca0
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-21 15:23:25 -0800

    Return Result Add 2 Int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result
+ *
+ * @a: First Parameter
+ * @b: Second Parameter
+ *
+ * Return: Result a + b
+ */
+
+int add(int a, int b)
+{
+	int add;
+
+	add = a + b;
+	return (add);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,4 @@ int print_last_digit(int);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int, int);
