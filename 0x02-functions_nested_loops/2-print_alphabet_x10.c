8340e2d5bcfec3f8d663cba01d523e2ad31f6f34
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-21 03:59:59 -0800

    Print Alphabet Prototype 10 Times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_alphabet -> for print lowercase alphabet
+ * print_alphabet -> for print lowercase alphabet 10 times
+ */
+
+void print_alphabet(void)
+{
+	char Start;
+
+	Start = 'a';
+	while (Start <= 'z')
+	{
+		_putchar(Start);
+		Start++;
+	}
+	_putchar('\n');
+}
+
+void print_alphabet_x10(void)
+{
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+	print_alphabet();
+}
