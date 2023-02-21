4f05b97069995f453cce7c4e69d2b7523e38d9c9
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-21 10:21:32 -0800

    Prints Last Digit Number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,19 +10,14 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0 && n != -2147483648)
-	{
-		n *= -1;
-		n %= 10;
-		_putchar(n + '0');
-	}
-	else if (n > 0)
+	if (n >= 0)
 	{
 		n %= 10;
 		_putchar(n + '0');
 	}
 	else
 	{
+		n *= -1;
 		n %= 10;
 		_putchar(n + '0');
 	}
