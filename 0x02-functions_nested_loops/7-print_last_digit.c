696f60fad37a81e8116173b55f1f892b5aca871b
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-21 09:57:18 -0800

    Prints Last Digit Number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit -> prints the last digit of a number
+ *
+ * @n: print_last_digit input
+ *
+ * Return: Always n (Success)
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		n *= -1;
+		n %= 10;
+		_putchar(n + '0');
+	}
+	else
+	{
+		n %= 10;
+		_putchar(n + '0');
+	}
+	return (n);
+}
