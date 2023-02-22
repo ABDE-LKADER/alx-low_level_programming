fca442ac2dfd0afac6194f2584d1dccbe20f080f
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-21 17:00:58 -0800

    Prints n Times Table, Starting 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,39 +10,35 @@ void print_times_table(int n)
 	int a;
 	int b;
 
-	if (n <= 15 && n > 0)
+	if (n <= 15 && n >= 0)
 	{
 		for (a = 0; a <= n; a++)
 		{
-			for (b = 0; b <= n; b++)
+			_putchar('0');
+			for (b = 1; b <= n; b++)
 			{
 				if (a * b <= 9)
 				{
-					if (b != 0)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-						_putchar(a * b + '0');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(a * b + '0');
 				}
 				else
 				{
+					_putchar(' ');
 					if (a * b <= 99)
 					{
 						_putchar(' ');
-						_putchar(' ');
 						_putchar((a * b) / 10 + '0');
-						_putchar((a * b) % 10 + '0');
 					}
 					else
 					{
-						_putchar(' ');
 						_putchar((a * b) / 100 + '0');
 						_putchar(((a * b) / 10) % 10  + '0');
-						_putchar((a * b) % 10 + '0');
 
 					}
+					_putchar((a * b) % 10 + '0');
 				}
 				if (b != n)
 					_putchar(',');
