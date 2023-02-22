109fa97a653bb529f85f2e856954bf115d19cfe3
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-21 17:29:56 -0800

    Multiples Of 3 Or 5 Sum

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 1024; a++)
+	{
+		if (a % 5 == 0 || a % 3 == 0)
+		{
+			b += a;
+		}
+	}
+	printf("%d\n", b);
+}
