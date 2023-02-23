7076719b0b53da1adf3cd7c08f5c3f51efa3e709
Abdelkader Adouab <82539204+ABDE-LKADER@users.noreply.github.com>
2023-02-23 14:41:40 +0100

    Create 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	unsigned long f1, f2, sum;
+
+	f1 = 0;
+	f2 = 1;
+	for (i = 0; i < 50; i++)
+	{
+		sum = f1 + f2;
+		printf("%lu", sum);
+
+		f1 = f2;
+		f2 = sum;
+
+		if (i == 49)
+			printf("\n");
+		else
+			printf(", ");
+	}
+
+	return (0);
+}
