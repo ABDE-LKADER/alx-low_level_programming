e98d148579e42649ca74e6da300c4a9f3a34e815
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-23 03:00:23 -0800

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,12 +12,12 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
 			printf("Buzz");
-		else if (i % 15 == 0)
-			printf("FizzBuzz");
 		else
 			printf("%d", i);
 		if (i != 100)
