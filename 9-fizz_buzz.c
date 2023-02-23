554d47ed504ef3ebff9ddef476f67fb872305806
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-23 01:11:58 -0800

    Fizz Or Buzz

---
+++ 9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -> Programm Fizz-Buzz
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
