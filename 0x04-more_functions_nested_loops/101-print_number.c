eba9ee784f3865058641af80a86ecac257c301c3
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-23 05:25:15 -0800

    Prints Integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,23 +8,12 @@
 
 void print_number(int n)
 {
-	if (n == -2147483648)
+	if (n < 0)
 	{
+		n *= -1;
 		_putchar('-');
-		_putchar('2');
-		print_number(147483648);	
 	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		n = -n;
-		print_number(n);
-	}
-	else  if (n > 9)
-	{
+	if (n / 10)
 		print_number(n / 10);
-		print_number(n % 10);
-	}
-	else
-		_putchar(n + '0');
+	_putchar((n % 10) + '0');
 }
