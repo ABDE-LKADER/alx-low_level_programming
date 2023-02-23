21942bd8249ddfbf3c3e1c5c303502cbbad362ce
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-22 23:55:59 -0800

    Checks For Digit 0 - 9 Without 2 and 4

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul -> Mult tow int
+ *
+ * @a: First int
+ * @b: Second int
+ *
+ * Return: Mult of a, b
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers -> print number from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
