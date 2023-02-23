e0d2f17d03b7495673b19b7642f5d7779f51b762
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-23 02:37:19 -0800

    Prints Triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,9 +1,34 @@
 #include "main.h"
 
 /**
+ * print_triangle -> Prints a triangle
+ *
+ * @size: Input
  */
 
 void print_triangle(int size)
 {
+	int i;
+	int j;
+	int k;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j >= 1; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
