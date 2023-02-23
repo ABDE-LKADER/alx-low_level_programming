cc29575700e62da815c0e4fdf5859537d545036d
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-23 00:12:34 -0800

    Draws A Straight Line In The Terminal

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line -> Draws a straight line in the terminal
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
