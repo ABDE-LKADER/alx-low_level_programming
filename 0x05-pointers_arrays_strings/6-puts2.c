4669b89023457d9dd2b96b4941f4e7ac4990ffb9
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-27 12:33:49 -0800

    Prints Every Other Character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 -> Prints every other character of a string
+ *
+ * @str: Input
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		_putchar('\n');
+	}
+}
