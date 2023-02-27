5fc67a744677665cf121eb7398b130b7f955e1a1
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-27 06:15:28 -0800

    Pwqrints String, Reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev -> Prints a string, in reverse
+ *
+ * @s: Input
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int j;
+	
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	for (j = i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+}
