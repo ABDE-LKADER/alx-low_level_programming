da3ae08098a5b0c10e100db865a02b51c478a534
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-27 00:47:04 -0800

    String Length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen -> The length of a string
+ *
+ * @s: Input
+ *
+ * Return: i String Length
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		continue;
+	}
+	return (i);
+}
