e7ddfa2ae55738c5651de527534649d5209527cb
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-02-27 17:24:51 -0800

    Generates Random Valid Passwords For The Program 101-crackme

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Generates random passwords 101-crackme
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int sum;
+	char ran;
+
+	srand(time(NULL));
+	while (sum <= 2645)
+	{
+		ran = rand() % 128;
+		sum += ran;
+		putchar(ran);
+	}
+	putchar(2772 - sum);
+	return (0);
+}
