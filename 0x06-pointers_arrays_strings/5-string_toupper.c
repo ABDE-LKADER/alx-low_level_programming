bdbb3cbedce5e5afaaf22391161108f836435ae1
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-01 15:15:24 -0800

    Uppercase Alpha

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *string_toupper ->  Changes all lowercase letters of a string to uppercase
+ *
+ * @str: Input String
+ *
+ * Return: String
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] > 'a' && str[i] < 'z')
+			str[i] -= 32;
+	}
+	return (str);
+}
