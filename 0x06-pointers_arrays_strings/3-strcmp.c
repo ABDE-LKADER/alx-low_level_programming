970cb07c7d8687cf758fcf27b7b75995a475b194
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-01 14:09:13 -0800

    Compare Tow String

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,7 +16,8 @@ int _strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] != '\0' || s2[i] != '\0')
 	{
-		return (s1[i] - s2[i]);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
 	return (0);
