97580d554a3964dffcfe0dc17990369656610c44
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-01 15:19:27 -0800

    Uppercase Alpha

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -14,7 +14,7 @@ char *string_toupper(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] >= 'a' && str[i] =< 'z')
+		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
 	}
 	return (str);
