0a0e327389bb12de474052005edba93c2ce950e4
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-01 11:32:51 -0800

    Copy A N String

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strncpy -> Copy Number Of String From Source Into Destinse
+ *
+ * @dest: Empty Input
+ * @src: Input Source
+ * @n: Input Number
+ *
+ * Return: Destinse String
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
