18447328df7c7dbbca2f893fef0658b3b028e7a9
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-01 16:58:20 -0800

    Capitalizes All Words Of String

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -16,10 +16,12 @@ char *cap_string(char *str)
 	{
 		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
-		if (str[i] == ' ' && str[i] == '\t' && str[i] == '\n' && str[i] == ',' && str[i] == ';' && str[i] == '.' && str[i] == '!' && str[i] == '?' && str[i] == '"' && str[i] == '(' && str[i] == ')' && str[i] == '{' && str[i] == '}')
+		else if (((str[i - 1] < 'A' || str[i - 1] > 'Z')
+				&& (str[i - 1] < 'a' || str[i - 1] > 'z')
+				&& (str[i - 1] < '0' || str[i - 1] > '9')))
 		{
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
-				str[i + 1] -= 32;
+			if (str[i] >= 'a' && str[i] <= 'z')
+				str[i] -= 32;
 		}
 	}
 	return (str);
