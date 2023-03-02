9be7c5dc14fdffbda04251a9bd73289b0c6b1643
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-01 18:39:15 -0800

    YET

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * *infinite_add -> Adds two numbers
+ *
+ * @n1: First Number
+ * @n2: Second Number
+ * @r: Result Stored
+ * @size_r: Buffer Size
+ *
+ * Return: 0 Must
+ */
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+	return (0);
+}
