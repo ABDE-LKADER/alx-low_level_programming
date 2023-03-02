1ee7ffe70a9a35057162e01bf7af776ec1d1eb41
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-01 18:25:16 -0800

    Magic Fixed

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -8,7 +8,6 @@ int main(void)
 
   a[2] = 1024;
   p = &n;
-
   /*
    * write your line of code here...
    * Remember:
@@ -19,7 +18,6 @@ int main(void)
    */
   p[5] = 98;
   /* ...so that this prints 98\n */
-
   printf("a[2] = %d\n", a[2]);
   return (0);
 }
