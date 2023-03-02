96144ec88bcaa0dc3d832354f0c3eef63f831b74
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-01 16:37:06 -0800

    Capitalizes All Words Of String

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * *cap_string ->  Capitalizes All Words Of String
+ * *cap_string ->  Capitalizes all words of string
  *
  * @str: Input String
  *
@@ -10,19 +10,21 @@
 
 char *cap_string(char *str)
 {
-	int i;
+	int	i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'A' && str[i] <= 'Z')
 			str[i] += 32;
 	}
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 1; str[i] != '\0'; i++)
 	{
-		if ((str[i - 1] < '0' || str[i - 1] == '9') && (str[ i- 1] < 'A' || str[i - 1] > 'Z') && (str[i - 1] < 'a' || str[i - 1] > 'z'))
+		if (((str[i - 1] < 'A' || str[i - 1] > 'Z')
+				&& (str[i - 1] < 'a' || str[i - 1] > 'z')
+				&& (str[i - 1] < '0' || str[i - 1] > '9'))
+				&& (str[i] >= 'a' && str[i] <= 'z'))
 			str[i] -= 32;
-		else
-			str[0] -= 32;
 	}
+	str[0] -= 32;
 	return (str);
 }
