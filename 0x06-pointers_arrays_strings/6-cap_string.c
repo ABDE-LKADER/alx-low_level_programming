7bc9947fd4b7020281a339483ce154bf0fc98390
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-01 16:54:29 -0800

    Capitalizes All Words Of String

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,15 +12,15 @@ char *cap_string(char *str)
 {
 	int	i;
 
-	for (i = 1; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[0] >= 'a' && str[0] <= 'z')
-			str[0] -= 32;
-		if (((str[i - 1] < 'A' || str[i - 1] > 'Z')
-				&& (str[i - 1] < 'a' || str[i - 1] > 'z')
-				&& (str[i - 1] < '0' || str[i - 1] > '9'))
-				&& (str[i] >= 'a' && str[i] <= 'z'))
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
+		if (str[i] == ' ' && str[i] == '\t' && str[i] == '\n' && str[i] == ',' && str[i] == ';' && str[i] == '.' && str[i] == '!' && str[i] == '?' && str[i] == '"' && str[i] == '(' && str[i] == ')' && str[i] == '{' && str[i] == '}')
+		{
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				str[i + 1] -= 32;
+		}
 	}
 	return (str);
 }
