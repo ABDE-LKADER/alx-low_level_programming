36b5d02accca425b10bd2c9b022112629d04ffb1
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-06 08:08:08 -0800

    Locate Character String

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,15 +12,11 @@
 char *_strchr(char *s, char c)
 {
 	int i;
-	int j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] == c)
-		{
-			for (j = i; s[j] != '\0'; j++)
-				return (s + j);
-		}
+			return (s + i);
 	}
 	return ('\0');
 }
