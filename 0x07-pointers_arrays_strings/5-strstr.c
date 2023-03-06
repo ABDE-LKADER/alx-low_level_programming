be673d1e851e09447574965e7954fa0e2ae81ad5
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-06 15:17:01 -0800

    Function That Locates A Substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strstr -> Locates a substring
+ *
+ * @haystack: Input Substring
+ * @needle: Input string
+ *
+ * Return: Depend Condition
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	if (*needle == '\0')
+		return (haystack);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[i + j] != '\0' && haystack[i + j] == needle[j]; j++);
+		if (needle[j] == '\0')
+			return (&haystack[i]);
+	}
+	return ('\0');
+}
