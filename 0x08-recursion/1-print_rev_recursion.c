05d95ef598d6c8ea12fed1313818d285c331a15a
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-08 01:43:31 -0800

    Prints A String In Reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,20 +8,10 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\n')
-	{
-		_putchar('\n');
-	}
 	if (*s != '\0')
 	{
 		s++;
 		_print_rev_recursion(s);
 		_putchar(*s);
-		s--;
-	}
-	if (s[0 + 1] == 'C')
-	{
-		_putchar('\0');
-		_putchar('\n');
 	}
 }
