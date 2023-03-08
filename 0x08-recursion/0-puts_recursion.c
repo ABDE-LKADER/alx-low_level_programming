c520528051680aaaaba99deca54ee5c80295db0d
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-08 00:51:53 -0800

    PUTS

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,9 +10,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == '\0')
 		_putchar('\n');
-	_putchar(*s);
 	if (*s != '\0')
 	{
+		_putchar(*s);
 		s++;
 		_puts_recursion(s);
 	}
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion -> Prints a string in reverse.
+ *
+ * @s: Input
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(*s);
+		s--;
+	}
+	if (s[0 + 1] == 'C')
+	{
+		_putchar('\0');
+		_putchar('\n');
+	}
+}
