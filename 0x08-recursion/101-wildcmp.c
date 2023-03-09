a064f0d49fa6a2e0a60b4278cf31f2b0bd89e78e
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-08 17:03:45 -0800

    Compares Two Strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,7 +15,7 @@ int wildcmp(char *s1, char *s2)
 		return (*s1 == '\0');
 	if (*s2 == *s1)
 	{
-		return (wildcmp(s1 + 1, s2 + 1));
+		return (*s1 != '\0' && wildcmp(s1 + 1, s2 + 1));
 	}
 	if (*s2 == '*')
 		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
