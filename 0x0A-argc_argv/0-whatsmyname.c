849f5c00767f9adf7cdced289be7a596f9e293a6
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-10 15:52:22 -0800

    Argv 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,7 @@
  * Return: 0 Always
  */
 
-int main (int argc __attribute__((unused)), char **argv)
+int main (int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
