9e02d09b0da424099fc618a1ddbfab69fc823d9a
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-13 11:23:56 -0700

    Allocate 2D Array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * **alloc_grid -> 2 dimensional array of integers
+ *
+ * @width: Input Gird With
+ * @height: Input Gird Height
+ *
+ * Return: Depend Condition
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int h;
+	int w;
+	int **sp;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	sp = (int **) malloc(height * sizeof(int *));
+	if (sp == NULL)
+		return (NULL);
+	for (h = 0; h < height; h++)
+	{
+		sp[h] = (int *) malloc(width * sizeof(int));
+		if (sp[h] == NULL)
+			return (NULL);
+	}
+	for (h = 0; h < height; h++)
+	{
+		for (w = 0; w < width; w++)
+		{
+			sp[h][w] = 0;
+		}
+	}
+	return (sp);
+}
