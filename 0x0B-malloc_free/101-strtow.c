42a67ac6377f5813fea91bde95da462a1247df4f
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-13 16:41:48 -0700

    Splits A String Into Words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,84 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _words -> Number Of Words
+ *
+ * @str: Input String
+ *
+ * Return: Number Words
+ */
+
+int _words(char *str)
+{
+	int i;
+	int j;
+	int si;
+
+	j = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ')
+			si = 0;
+		else if (si == 0)
+		{
+			si = 1;
+			j++;
+		}
+	}
+	return (j);
+}
+
+/**
+ * **strtow -> Splits a string into words
+ *
+ * @str: Input String
+ */
+
+char **strtow(char *str)
+{
+	char **new;
+	char *res;
+	int i;
+	int j;
+	int k;
+	int End;
+	int Start;
+	int len;
+	int words;
+
+	for (len = 0; str[len] != '\0'; len ++)
+		;
+	words = _words(str);
+	if (words == 0)
+		return (NULL);
+
+	new = (char **) malloc(words + 1 * sizeof(char *));
+	if (new == NULL)
+		return (NULL);
+	k = 0;
+	j = 0;
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (j)
+			{
+			End = i;
+			res = (char *) malloc(j + 1 * sizeof(char));
+			if (res == NULL)
+				return (NULL);
+			while (Start < End)
+				*res++ = str[Start++];
+			*res = '\0';
+			new[k] = res - j;
+			k++;
+			j = 0;
+			}
+		}
+		else if (j++ == 0)
+			Start = i;
+	}
+	new[k] = NULL;
+	return (new);
+}
