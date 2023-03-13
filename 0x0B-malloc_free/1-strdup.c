f62048596d1622998f6b66b57f5c26be2729a678
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-13 09:07:04 -0700

    Allocate Space Newly

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,8 +2,32 @@
 #include "main.h"
 
 /**
+ * *_strdup -> Newly allocated space in memory
+ *
+ * @str: Input String
+ *
+ * Return: Depend Condition
  */
 
 char *_strdup(char *str)
 {
+	int i;
+	int len;
+	char *new;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if (len == 0)
+		return (NULL);
+	new = (char *) malloc(len * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+	{
+		new[i] = str[i];
+	}
+	return (new);
 }
