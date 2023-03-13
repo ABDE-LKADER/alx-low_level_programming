f8121d14df986094ebd18c8041ea1fcaef8ead88
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-13 08:33:54 -0700

    Allocate Space And Initializes It

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,9 +15,9 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
-	arr = (char *) malloc(size * sizeof(char));
 	if (size == 0)
 		return (NULL);
+	arr = (char *) malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
