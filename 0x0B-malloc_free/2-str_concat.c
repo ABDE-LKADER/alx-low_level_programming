b978b0179eaa7cc294a30442ff5c535271fe6f98
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-13 10:30:26 -0700

    Allocate Space Of String 1 Folowed By String 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,6 +20,10 @@ char *str_concat(char *s1, char *s2)
 	int len2;
 	char *new;
 
+	if (s1 == NULL)
+		s1 = '\0';
+	if (s2 == NULL)
+		s2 = '\0';
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
 	for (len2 = 0; s2[len2] != '\0'; len2++)
