20a97be58a0f8b1d9546b703e364729e5637a877
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-16 18:31:07 -0700

    Allocates Memory Array

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,7 +2,29 @@
 #include "main.h"
 
 /**
+ * *_calloc -> Allocates memory for an array
+ *
+ * @nmemb: Input Array
+ * @size: Size Elements
+ *
+ * Return: Depend Condition
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
-{}
+{
+	unsigned int i;
+	unsigned int m;
+	void *a;
+	char *b;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	m = nmemb * size;
+	a = (void *) malloc(m);
+	if (a == NULL)
+		return (NULL);
+	b = a;
+	for (i = 0; i < m; i++)
+		b[i] = '\0';
+	return (a);
+}
