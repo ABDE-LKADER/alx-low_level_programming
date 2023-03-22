6577c47ffd80c8369acee133c93e43c131bfe483
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-21 18:50:21 -0700

    Define Struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Dog attributes
+ *
+ * @name: The Name
+ * @age: The Age
+ * @owner: The Owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 
 #endif
