50ab5d186bc4e6fd0063285d75fe7348b300abb4
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-22 16:40:01 -0700

    Prints Name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name -> Prints a name
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTION_POINTTERS_H
+#define FUNCTION_POINTTERS_H
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
