15fa1f29c5cba1a83269990432e6456ed12a3067
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-22 16:52:05 -0700

    Executes Function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,23 @@
-#include ""
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator -> Excuted Function
+ *
+ * @array: Input Array
+ * @size: Array Size
+ * @action: Pointer To Function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (array != NULL || action != NULL)
+	{
+		for (i = 0; i < (int)size; i++)
+			action(array[i]);
+	}
+	else
+		return;
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_POINTTERS_H
 #define FUNCTION_POINTTERS_H
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
