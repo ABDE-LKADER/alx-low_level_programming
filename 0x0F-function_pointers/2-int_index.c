ee4d7ff54e7176022712979590e5fc18dbd968b9
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-22 17:13:27 -0700

    Searches For Integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+
 /**
  * int_index - searches for an integer
  * @array: array with data
@@ -6,13 +7,12 @@
  * @cmp: function pointer
  * Return: count of integer
  */
+
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 
-if (size <= 0)
-return (-1);
-if (array == NULL || cmp == NULL)
+if (size <= 0 || array == NULL || cmp == NULL)
 return (-1);
 
 for (i = 0; i < size; i++)
