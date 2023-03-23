88af78a7a82b5b15eeaa9bbef7e0ad6889e3b81d
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-22 17:14:46 -0700

    Searches For Integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,15 +10,14 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i;
+	int int_index;
 
-if (size <= 0 || array == NULL || cmp == NULL)
-return (-1);
-
-for (i = 0; i < size; i++)
-{
-if (cmp(array[i]) == 1)
-return (i);
-}
-return (-1);
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 1)
+			return (int_index);
+	}
+	return (-1);
 }
