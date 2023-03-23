4c708abfeceaf035909dd9f52cb8f63210977290
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-22 17:09:57 -0700

    Searches For Integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index -> Searches for an integer
+ *
+ * @array: Input Array
+ * @size: Array Size
+ * @cmp: Pointer To Function
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int int_index;
+
+	if (size <= 0 || array == NULL || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i] == 1))
+			return (int_index);
+	}
+	return (-1);
+}
