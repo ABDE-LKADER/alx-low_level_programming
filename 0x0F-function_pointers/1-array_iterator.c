b2426313877d0d1022b3d95bf551f851b236419c
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-22 18:24:35 -0700

    Executes Function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,10 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	int i;
 
 	if (array != NULL || action != NULL)
-	{
-		for (i = 0; i < (int)size; i++)
+		return:
+	for (i = 0; i < (int)size; i++)
 			action(array[i]);
-	}
-	else
-		return;
 }
