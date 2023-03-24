95325de869240875ba1a5975270112f82b47e08f
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-24 10:17:53 -0700

    FIX Args Sum

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,15 +14,14 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int sum = 0;
 	va_list list;
-
-	if (n)
+	
+	va_start(list, n);
+	if (n == 0)
+		return (0);
+	for (i = 0; i < n; i++)
 	{
-		va_start(list, n);
-		for (i = 0; i < n; i++)
-		{
-			sum += va_arg(list, const unsigned int);
-		}
-		va_end(list);
+		sum += va_arg(list, const unsigned int);
 	}
+	va_end(list);
 	return (sum);
 }
