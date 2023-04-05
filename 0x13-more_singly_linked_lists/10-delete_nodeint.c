341ca55b9024b5cf21400e4fdd03056d8351529b
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-04-05 02:49:26 +0000

    Delete Node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Deletes the node at index index of a listint_t
+ *
+ * @head: Input list
+ * @index: Index Node
+ *
+ * Return: Depend Condition
+*/
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *T, *T2;
+	unsigned int i;
+
+	if (head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		T = *head;
+		*head = T->next;
+		free(T);
+		return (1);
+	}
+	T = *head;
+	for (i = 0; i < index - 1; i++)
+	{
+		if (T == NULL)
+			return (-1);
+		T = T->next;
+	}
+	T2 = T->next;
+	T->next = T2->next;
+	free(T2);
+	return (1);
+}
