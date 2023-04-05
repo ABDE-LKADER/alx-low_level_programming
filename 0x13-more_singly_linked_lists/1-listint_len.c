49664ab49194fbb3831eab848271e92f414fe946
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-04-05 00:54:53 +0000

    Number Elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len -> returns the number of elements in a linked listint_t list
+ * 
+ * @h: Input list
+ * 
+ * Return: Nodes Number
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes;
+
+	for (nodes = 0; h != '\0'; nodes++)
+		h = h->next;
+	return (nodes);
+}
