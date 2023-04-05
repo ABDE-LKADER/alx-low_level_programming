25cedb400da2f52e74233e8afb0a09a18060c870
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-04-05 02:30:22 +0000

    Free Lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,42 +8,12 @@
 
 void free_listint(listint_t *head)
 {
-    listint_t *free_list;
+	listint_t *free_list;
 
-    while (head != NULL)
-    {
-        free_list = head;
-        head = head->next;
-        free(free_list);
-    }
-}
-
-
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include "lists.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    listint_t *head;
-
-    head = NULL;
-    add_nodeint_end(&head, 0);
-    add_nodeint_end(&head, 1);
-    add_nodeint_end(&head, 2);
-    add_nodeint_end(&head, 3);
-    add_nodeint_end(&head, 4);
-    add_nodeint_end(&head, 98);
-    add_nodeint_end(&head, 402);
-    add_nodeint_end(&head, 1024);
-    print_listint(head);
-    free_listint(head);
-    head = NULL;
-    return (0);
+	while (head != NULL)
+	{
+		free_list = head;
+		head = head->next;
+		free(free_list);
+	}
 }
