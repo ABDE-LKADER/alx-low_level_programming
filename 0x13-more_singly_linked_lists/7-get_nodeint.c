5d8296751c3753d393ca586b5d0cd21cccd7e794
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-04-05 02:39:17 +0000

    NTH Mode

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index -> Returns the nth node of a listint_t linked list.
+ * 
+ * @head: Input list
+ * @index: Index Node
+ * 
+ * Return: Node
+*/
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; i++)
+	{
+		if (head->next == NULL)
+			return (NULL);
+		head = head->next;
+	}
+	return (head);
+}
