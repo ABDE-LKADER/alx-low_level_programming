daa1ee0880e75f1a09b99ab41526688f264b1a16
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-04-06 03:08:11 +0000

    Prints listint_t

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,21 +10,24 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t i = 0;
+	size_t i;
 	long int T;
 
+	if (head == NULL)
+		exit(98);
+	i = 0;
 	while (head)
 	{
 		T = head - head->next;
 		i++;
 		printf("[%p] %d\n", (void *)head, head->n);
-		if (T > 0)
-			head = head->next;
-		else
+		if (T <= 0)
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			break;
 		}
+		else
+			head = head->next;
 	}
 	return (i);
 }
