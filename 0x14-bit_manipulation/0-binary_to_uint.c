f0d95056018e2451f9ac65419820422aee5ee65f
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-04-11 01:47:02 +0000

    Converts Binary Into Int Number

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,21 +2,21 @@
 
 /**
  * binary_to_uint -> Converts a binary number to unsigned int
- *
+ * 
  * @b: Input String Binary
- *
+ * 
  * Return: Depending Condition
 */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, len, num = 0;
+	unsigned int i, len, num;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 	for (len = 0; b[len] != '\0'; len++)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[len] < '0' && b[len] > '1')
 			return (0);
 	}
 	for (i = 0; i < len; i++)
