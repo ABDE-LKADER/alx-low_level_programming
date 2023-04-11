a5cb9e985f44ef9dd8ebf7e3f2beccc2b76ac1d9
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-04-11 02:34:20 +0000

    Clear Bit --> Index

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,6 @@ void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif
