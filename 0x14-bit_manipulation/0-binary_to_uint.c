e4aa711f9f65f883b0337188150e61e5f252d345
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-04-11 01:37:32 +0000

    Converts Binary Into Int Number

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		num = num * 2 + b[i] - '0';
+		num = num * 2 + (b[i] - '0');
 	}
 	return (num);
 }
