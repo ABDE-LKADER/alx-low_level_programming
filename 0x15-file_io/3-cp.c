081b093f49374300f3a10595a40bf91b6fe28fbd
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-04-15 02:59:14 +0000

    Copies Content Of File

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,34 +3,52 @@
 /**
  * main - Entry point
  *
+ * @argc: Input Number of Arguments
+ * @argv: Input Arguments
+ *
  * Return: Always 0
- */
+*/
 
 int main(int argc, char *argv[])
 {
-	int fd;
-	ssize_t nletters;
-	char *filename;
-	char *text_content;
+	int f_rd, f_wr, i, j, k;
+	char buffer[BUFSIZ];
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	filename = argv[1];
-	text_content = argv[2];
-	nletters = 0;
-	printf("-> %s file created\n", filename);
-	fd = create_file(filename, text_content);
-	printf("-> %s file written\n", filename);
-	nletters = read_textfile(filename, 1024);
-	printf("-> %s file read\n", filename);
-	printf("-> %ld bytes read\n", nletters);
-	printf("-> %s file appended\n", filename);
-	nletters = append_text_to_file(filename, "\nI love C!");
-	printf("-> %ld bytes appended\n", nletters);
-	nletters = read_textfile(filename, 1024);
-	printf("-> %ld bytes read\n", nletters);
+	f_rd = open(argv[1], O_RDONLY);
+	if (f_rd < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	f_wr = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((i = read(f_rd, buffer, BUFSIZ)) > 0)
+	{
+		if (f_wr < 0 || write(f_wr, buffer, i) != i)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(f_rd);
+			exit(99);
+		}
+	}
+	if (i < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	j = close(f_rd);
+	k = close(f_wr);
+	if (j < 0 || k < 0)
+	{
+		if (j < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_rd);
+		if (k < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f_wr);
+		exit(100);
+	}
 	return (0);
 }
