ea4b2ffb669ac9e5cdfe96d8afa554afe676ce5e
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-04-15 02:47:51 +0000

    Copies Content Of File

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    int fd;
+    ssize_t nletters;
+    char *filename;
+    char *text_content;
+
+    filename = "test_0.txt";
+    text_content = "Holberton School!\n";
+    nletters = 0;
+    printf("-> %s file created\n", filename);
+    fd = create_file(filename, text_content);
+    printf("-> %s file written\n", filename);
+    nletters = read_textfile(filename, 1024);
+    printf("-> %s file read\n", filename);
+    printf("-> %ld bytes read\n", nletters);
+    printf("-> %s file appended\n", filename);
+    nletters = append_text_to_file(filename, "\nI love C!");
+    printf("-> %ld bytes appended\n", nletters);
+    nletters = read_textfile(filename, 1024);
+    printf("-> %ld bytes read\n", nletters);
+    return (0);
+}
