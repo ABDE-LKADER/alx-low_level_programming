998ea692435a99db3f364ebbfbbf52a68c9029ba
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-19 03:21:34 +0100

    List SUM

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint -> Returns the sum of all the data (n) of a dlistint_t linked list
+ * @head: Head List
+ * Return: Sum
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+	for (; head != NULL; head = head->next)
+		sum += head->n;
+	return (sum);
+}
