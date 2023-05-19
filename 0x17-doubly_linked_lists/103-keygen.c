b6f44d2e6c1fb14ee11e22305c059c29543354d8
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-19 03:54:02 +0100

    Crackme 5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -3,31 +3,139 @@
 #include <time.h>
 
 /**
- * main -> Keygen for crackme5
- * @argc: Args Number
- * @argv: Args Array
- * Return: Depend Condition
+ * find_first -> Finds the biggest number
+ * @array: Int Array
+ * @size: Array Size
+ * Return: Biggest Number
  */
 
-int main(int argc, char *argv[])
+int find_first(int *array, int size)
 {
-	char *username = argv[1];
-	int len = 0, i = 0, sum = 0, mul = 1, first = 0, second = 0;
-	time_t t;
+	int i, first = 0;
 
-	if (argc != 2)
-		return (-1);
-	while (username[len])
-		len++;
-	srand((unsigned int) time(&t));
-	first = rand() & 0x3f;
-	second = rand() & 0x3f;
-	printf("%c%c", first, second);
-	for (i = 0; i < len; i++)
+	for (i = 0; i < size; i++)
 	{
-		sum += username[i];
-		mul *= username[i];
+		if (array[i] > first)
+			first = array[i];
 	}
-	printf("%c%c\n", sum, mul);
+	return (first);
+}
+
+/**
+ * find_second -> Finds the second biggest number
+ * @array: Int Array
+ * @size: Array Size
+ * Return: Second Biggest Number
+ */
+
+int find_second(int *array, int size)
+{
+	int i, first = 0, second = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > first)
+		{
+			second = first;
+			first = array[i];
+		}
+		else if (array[i] > second && array[i] != first)
+			second = array[i];
+	}
+	return (second);
+}
+
+/**
+ * find_third -> Finds the third biggest number
+ * @array: Int Array
+ * @size: Array Size
+ * Return: Third Biggest Number
+ */
+
+int find_third(int *array, int size)
+{
+	int i, first = 0, second = 0, third = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > first)
+		{
+			third = second;
+			second = first;
+			first = array[i];
+		}
+		else if (array[i] > second && array[i] != first)
+		{
+			third = second;
+			second = array[i];
+		}
+		else if (array[i] > third && array[i] != second && array[i] != first)
+			third = array[i];
+	}
+	return (third);
+}
+
+/**
+ * find_fourth -> Finds the fourth biggest number
+ * @array: Int Array
+ * @size: Array Size
+ * Return: Fourth Biggest Number
+ */
+
+int find_fourth(int *array, int size)
+{
+	int i, first = 0, second = 0, third = 0, fourth = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > first)
+		{
+			fourth = third;
+			third = second;
+			second = first;
+			first = array[i];
+		}
+		else if (array[i] > second && array[i] != first)
+		{
+			fourth = third;
+			third = second;
+			second = array[i];
+		}
+		else if (array[i] > third && array[i] != second && array[i] != first)
+		{
+			fourth = third;
+			third = array[i];
+		}
+		else if (array[i] > fourth && array[i] != third && array[i] != second
+			 && array[i] != first)
+			fourth = array[i];
+	}
+	return (fourth);
+}
+
+/**
+ * main -> Generates a random password
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i, first, second, third, fourth, sum = 0, size = 0;
+	int array[100];
+
+	srand(time(NULL));
+	while (sum < 2772)
+	{
+		array[size] = rand() % 100;
+		sum += array[size];
+		size++;
+	}
+	sum -= 2772;
+	array[size - 1] -= sum;
+	first = find_first(array, size);
+	second = find_second(array, size);
+	third = find_third(array, size);
+	fourth = find_fourth(array, size);
+	printf("%c%c%c%c\n", first, second, third, fourth);
 	return (0);
 }
