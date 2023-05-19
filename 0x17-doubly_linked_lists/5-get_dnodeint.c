bfaffe75cd32fd44e0d3edbd720fcc1dc783193a
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-19 03:19:35 +0100

    NTH Node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index -> Returns the nth node of a dlistint_t linked list
+ * @head: Head List
+ * @index: Index Node
+ * Return: Node
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    unsigned int i;
+    dlistint_t *a;
+
+    if (head == NULL)
+        return (NULL);
+    a = head;
+    for (i = 0; i < index; i++)
+    {
+        if (a == NULL)
+            return (NULL);
+        a = a->next;
+    }
+    return (a);
+}
