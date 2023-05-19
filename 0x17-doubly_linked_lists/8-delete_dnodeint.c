bc1a9a2bf18c13e9069b926259479d050aa5d77a
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-19 03:26:50 +0100

    DELETE Index

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index -> Deletes the node at index index of a dlistint_t linked list
+ * delete_dnodeint_at_index -> Deletes node at index a dlistint_t linked list
  * @head: Head List
  * @index: Index Node
  * Return: Depend Condition
