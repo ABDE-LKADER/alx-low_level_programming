5fe0de1d63567a4937d6f5d50cf72adf25a953be
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-19 02:39:02 +0100

    Print ALL

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,7 +10,9 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i;
 	
-	for (i = 0; h->prev != NULL; i++)
+	if (h == NULL)
+		return (count);
+	while (h->prev != NULL)
 		h = h->prev;
 	for (i = 0; h != NULL; i++)
 	{
