de8d06341013c04241d4e783585ee4994ea61217
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-19 03:13:19 +0100

    Add New To END

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -9,28 +9,28 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new, *a;
+	dlistint_t *new, *a;
 
-    if (head == NULL)
-        return (NULL);
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-        return (NULL);
-    new->n = n;
-    new->prev = NULL;
-    new->next = NULL;
-    if (*head == NULL)
-    {
-        *head = new;
-        return (new);
-    }
-    else
-    {
-        a = *head;
-        while (a->next != NULL)
-            a = a->next;
-        a->next = new;
-        new->prev = a;
-        return (new);
-    }
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	else
+	{
+		a = *head;
+		while (a->next != NULL)
+			a = a->next;
+		a->next = new;
+		new->prev = a;
+		return (new);
+	}
 }
