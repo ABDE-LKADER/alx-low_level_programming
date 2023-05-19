b20c05bbe451ce538e52498b1935218dfb8603b9
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-19 03:24:09 +0100

    NTH Node

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -> Returns the sum of all the data (n) of a dlistint_t linked list
+ * sum_dlistint -> Returns the sum of all the n a dlistint_t linked list
  * @head: Head List
  * Return: Sum
  */
