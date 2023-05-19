9c841620519bd284eec830221e779ee5185aad9d
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-19 03:16:14 +0100

    Free List

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlistint -> Frees a dlistint_t list
+ * @head: Head List
+*/
+
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *a;
+
+    while (head != NULL)
+    {
+        a = head;
+        head = head->next;
+        free(a);
+    }
+}
