af4879f3bd82131a92f3c266933080641ac5db8d
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-23 03:08:56 +0100

    Dynamic LIB SCRIPT

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# Compile each .c file into object files
 gcc -c -fPIC *.c
-gcc -shared *.o -o liball.so
+
+# Create the dynamic library
+gcc -shared -o liball.so *.o
+
+# Cleanup: remove the object files
 rm *.o
