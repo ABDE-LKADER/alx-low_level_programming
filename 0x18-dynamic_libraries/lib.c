3da25d65c573609bc79ceb268a18a987bca2dca6
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-23 04:38:54 +0100

    GM Make Me Win

--- 0x18-dynamic_libraries/lib.c
+++ 0x18-dynamic_libraries/lib.c
@@ -10,6 +10,7 @@
 
 int printf(const char *format, ...)
 {
-	write(1, "--> Please make me win!\n", 25);
+	write(1, "9 8 10 24 75 - 9\n", 18);
+	write(1, "Congratulations, you win the Jackpot!\n", 39);
 	exit(EXIT_SUCCESS);
 }
