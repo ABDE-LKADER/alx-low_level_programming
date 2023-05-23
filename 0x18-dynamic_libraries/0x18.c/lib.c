b338330a6818d2e50501e96aec2d421bb0cd57b4
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-23 04:14:31 +0100

    GM Make Me Win

--- 0x18-dynamic_libraries/0x18.c/lib.c
+++ 0x18-dynamic_libraries/0x18.c/lib.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/**
+ * printf -> Print the "Please make me win!" message
+ * @format: String Format
+ * Return: 0
+*/
+
 int printf(const char *format, ...)
 {
 	write(1, "--> Please make me win!\n", 25);
