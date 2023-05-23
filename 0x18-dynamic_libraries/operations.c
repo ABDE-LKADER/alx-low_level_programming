f75042b519cdfeb8382430997ac6b85aebeb4257
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-23 03:21:56 +0100

    Call Lib With Python

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,61 @@
+#include "operations.h"
+
+/**
+ * add -> Adds two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Sum of a and b
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub -> Subtracts two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Difference of a and b
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul -> Multiplies two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Product of a and b
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div -> Divides two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Quotient of a and b
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod -> Modulus of two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: Remainder of a and b
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
