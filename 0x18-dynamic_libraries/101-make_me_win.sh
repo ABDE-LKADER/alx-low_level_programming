e3bc18dc75dfe311eae7ae3de1a76db3ed55bac2
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-05-23 03:44:45 +0100

    GM

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://raw.github.com/Pericles001/alx-low_level_programming/master/0x18-dynamic_libraries/nrandom.so
-export LD_PRELOAD=/$PWD/lib.so
+wget -P /$PWD/ https://raw.github.com/Pericles001/alx-low_level_programming/master/0x18-dynamic_libraries/lib.so
+export LD_PRELOAD =/$PWD/lib.so
