7137670b47ab68cc6e324e34f559e949f024d88d
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-10 03:50:23 +0100

    0x1A. C -> Hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,7 +8,9 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash = hash_djb2(key);
-	unsigned long int index = hash % size;
-	return (index);
+	unsigned long int index = 0;
+
+    index = hash_djb2(key);
+    index = index % size;
+    return (index);
 }
---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,45 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set -> Adds an element to the hash table.
+ * @ht: Hash Table
+ * @key: Hash Table Key
+ * @value: Hash Table Value
+ * Return: Depend Condition
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = 0;
+	hash_node_t *new_node = NULL, *temp = NULL;
+
+	if (ht == NULL || key == NULL || value == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			free(temp->value);
+			temp->value = strdup(value);
+			if (temp->value == NULL)
+				return (0);
+			return (1);
+		}
+		temp = temp->next;
+	} new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	if (new_node->key == NULL || new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node->value);
+		free(new_node);
+		return (0);
+	} new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
