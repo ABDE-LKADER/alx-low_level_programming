be551eb50c7ef0fdcf6f40dce1eb63b3ce2f010c
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-10 03:52:00 +0100

    0x1A. C -> Hash tables

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print -> Prints a hash table.
+ * @ht: Hash Table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *temp = NULL;
+	int flag = 0;
+
+	if (ht == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", temp->key, temp->value);
+			flag = 1;
+			temp = temp->next;
+		}
+	} printf("}\n");
+}
