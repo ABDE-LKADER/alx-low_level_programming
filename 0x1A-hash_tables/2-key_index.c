46f28bf2800d7ee11d030f890b786b99b3770052
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-06-15 01:50:47 +0100

    0x1A. C -> Hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,5 +8,7 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key) % size);
+	unsigned long int index = hash_djb2(key) % size;
+
+	return (index);
 }
