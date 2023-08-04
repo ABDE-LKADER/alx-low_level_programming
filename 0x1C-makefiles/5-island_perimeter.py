41dc2165fc1ee88897832e9093084f20dbd2df9d
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-08-04 02:01:03 +0100

    C - Makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
+"""Defines an island perimeter measuring function"""
+
 
 def island_perimeter(grid):
     """Calculate the perimeter of the island described in the grid"""
-    perimeter = 0
 
-    if not grid:
-        return perimeter
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-    rows, cols = len(grid), len(grid[0])
-
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                perimeter += 4
-                if row > 0 and grid[row - 1][col] == 1:
-                    perimeter -= 2
-                if col > 0 and grid[row][col - 1] == 1:
-                    perimeter -= 2
-
-    return perimeter
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
